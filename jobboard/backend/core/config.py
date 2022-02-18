6ffa4159349962e4d313ee26b899e36bf114e9ba
Jansen A. Simanullang <jansen.simanullang@gmail.com>
2022-02-18 16:51:35 +0700

    Update config.py
    package for databases

--- jobboard/backend/core/config.py
+++ jobboard/backend/core/config.py
@@ -1,5 +1,12 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+env_path = Path('.') / '.env'
+
 class settings:
-    PROJECT_TITLE : str = "Jobboard"
+    PROJECT_TITLE: str = "Jobboard"
     PROJECT_VERSION: str = "0.1.1"
+
 
 settings = Settings()
