2132a3c4f44f37b470b9ddde1cd93526bd156bc3
Jansen A. Simanullang <jansen.simanullang@gmail.com>
2022-02-21 13:56:27 +0700

    Update main.py

--- jobboard/backend/main.py
+++ jobboard/backend/main.py
@@ -1,9 +1,27 @@
 from fastapi import FastAPI
-from core.config import settings
+from jobboard.backend.core.config import settings
+from jobboard.backend.db.session import engine
+from jobboard.backend.db.base_class import Base
 
-app = FastAPI(  title=settings.PROJECT_TITLE, 
-                version= settings.PROJECT_VERSION)
+
+def create_tables():
+
+    Base.metadata.create_all(bind=engine)
+
+
+def start_application():
+
+    app = FastAPI(title=settings.PROJECT_TITLE,
+                  version=settings.PROJECT_VERSION)
+
+    create_tables()
+
+    return app
+
+
+app = start_application()
+
 
 @app.get("/")
 def hello_api():
-    return {"detail":"Hello World!"}
+    return {"detail": "Hello World!"}
