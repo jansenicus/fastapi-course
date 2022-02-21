7f34b0bf0a7c3561a2cc63e5ee30e6ba488eeb91
Jansen A. Simanullang <jansen.simanullang@gmail.com>
2022-02-21 16:14:37 +0700

    Update session.py

--- jobboard/backend/db/session.py
+++ jobboard/backend/db/session.py
@@ -1,17 +1,27 @@
-import imp
+from typing import Generator
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from jobboard.backend.core.config import settings
 
-# SQL_ALCHEMY_DATABASE_URL = settings.DATABASE_URL
-# engine = create_engine(SQL_ALCHEMY_DATABASE_URL)
+SQL_ALCHEMY_DATABASE_URL = settings.DATABASE_URL
+engine = create_engine(SQL_ALCHEMY_DATABASE_URL)
 
-SQL_ALCHEMY_DATABASE_URL = 'sqlite:///./sql_app.db'
-engine = create_engine( SQL_ALCHEMY_DATABASE_URL,
-                        connect_args={'check_same_thread':
-                                     False})
+# SQL_ALCHEMY_DATABASE_URL = 'sqlite:///./sql_app.db'
+# engine = create_engine( SQL_ALCHEMY_DATABASE_URL,
+#                        connect_args={'check_same_thread':
+#                                     False})
 
 SessionLocal = sessionmaker(autocommit=False,
                             autoflush=False,
                             bind=engine)
+
+def get_db() -> Generator:
+    """
+    returns a generator
+    """
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
