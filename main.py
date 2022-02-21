0536b3a18acd00a491540987a8c65871780245ca
Jansen A. Simanullang <jansen.simanullang@gmail.com>
2022-02-21 13:53:59 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import uvicorn
 
 if __name__ == '__main__':
     uvicorn.run('jobboard.backend.main:app',
-                    host='127.0.0.1',
-                    port=8000, 
-                    log_level='info')
+                host='127.0.0.1',
+                port=8000,
+                log_level='info')
