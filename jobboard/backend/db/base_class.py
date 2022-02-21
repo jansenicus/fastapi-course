9b8da30a2f3ffa9496ce3edee8a77293a0385dd3
Jansen A. Simanullang <jansen.simanullang@gmail.com>
2022-02-21 16:14:13 +0700

    Update base_class.py

--- jobboard/backend/db/base_class.py
+++ jobboard/backend/db/base_class.py
@@ -1,5 +1,6 @@
 from typing import Any
-from sqlalchemy.ext.declarative import (as_declarative, declared_attr)
+from sqlalchemy.ext.declarative import (as_declarative,
+                                        declared_attr)
 
 
 @as_declarative()
@@ -7,5 +8,6 @@ class Base:
     id: Any
     __name__: str
 
+    @declared_attr
     def __tablename__(cls) -> str:
         return cls.__name__.lower()
