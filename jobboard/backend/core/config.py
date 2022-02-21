0f8df3c95a2dbfe76999b1b9d5b529dfbc4d6d6d
Jansen A. Simanullang <jansen.simanullang@gmail.com>
2022-02-21 13:54:57 +0700

    dotenv_values
    using dotenv_values instead of load_env

--- jobboard/backend/core/config.py
+++ jobboard/backend/core/config.py
@@ -1,12 +1,20 @@
 import os
 from pathlib import Path
-from dotenv import load_dotenv
+from dotenv import dotenv_values
+env_path = Path(".")/'.env'
+config = dotenv_values(env_path)
 
-env_path = Path('.') / '.env'
 
-class settings:
+class Settings:
+
     PROJECT_TITLE: str = "Jobboard"
     PROJECT_VERSION: str = "0.1.1"
+    POSTGRES_USER: str = config.get("POSTGRES_USER")
+    POSTGRES_PASSWORD: str = config.get("POSTGRES_PASSWORD")
+    POSTGRES_SERVER: str = config.get("POSTGRES_SERVER")
+    POSTGRES_PORT: str = config.get("POSTGRES_PORT")
+    POSTGRES_DB: str = config.get("POSTGRES_DB")
+    DATABASE_URL: str = f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}'
 
 
 settings = Settings()
