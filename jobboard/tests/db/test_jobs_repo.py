f781d1930bd3525970b02492b22dc79fdda4eae3
Jansen A. Simanullang <jansen.simanullang@gmail.com>
2022-02-22 18:20:07 +0700

    Create test_jobs_repo.py

---
+++ jobboard/tests/db/test_jobs_repo.py
@@ -0,0 +1,26 @@
+from sqlalchemy.orm import Session
+
+from backend.db.repository.jobs import (create_new_job, retrieve_job)
+from backend.schemas.jobs import JobCreate
+from tests.utils.user import create_random_owner
+
+def test_retrieve_job_by_id(db_session: Session):
+    title = "test-title"
+    company = "test comp"
+    company_url = "test.com"
+    location = "NY, USA"
+    description = "Foo Bar"
+    owner = create_random_owner(db = db_session)
+    job_schema = JobCreate(title=title,
+                            company=company, 
+                            company_url=company_url,
+                            location=location,
+                            description=description)
+
+    job = create_new_job(job = job_schema, db=db_session, owner_id=owner.id)
+    retrieved_job = retrieve_job(id = job.id, 
+                                    db = db_session)
+    assert retrieved_job.id == job.id
+    assert retrieved_job.title == "test-title"
+
+
