fb72d6aff9bfe93202d81dc230699844a3ce4662
Jansen A. Simanullang <jansen.simanullang@gmail.com>
2022-02-22 18:20:21 +0700

    Update test_users.py
    singular form user

--- jobboard/tests/test_routes/test_users.py
+++ jobboard/tests/test_routes/test_users.py
@@ -3,12 +3,12 @@ import json
 
 def test_create_user(client):
     data = {
-        "username": "testusername",
+        "username": "test",
         "email": "abc@test.com",
         "password": "1234567",
     }
 
-    response = client.post("/users/",
+    response = client.post("/user/",
                            json.dumps(data))
 
     assert response.status_code == 200
