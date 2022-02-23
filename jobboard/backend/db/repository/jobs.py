5a63f807ad7fb3e5de8f36de6876a9068cdc0c1b
Jansen A. Simanullang <jansen.simanullang@gmail.com>
2022-02-23 19:17:22 +0700

    Update jobs.py
    added list_jobs

--- jobboard/backend/db/repository/jobs.py
+++ jobboard/backend/db/repository/jobs.py
@@ -3,19 +3,25 @@ from sqlalchemy.orm import Session
 from backend.schemas.jobs import JobCreate
 from backend.db.models.jobs import Job
 
+
 def create_new_job(job: JobCreate, db: Session, owner_id: int):
-    job = Job(**job.dict(), owner_id = owner_id)
+    job = Job(**job.dict(), owner_id=owner_id)
     db.add(job)
     db.commit()
     db.refresh(job)
-
     return job
-    
+
+
 def retrieve_job(id: int, db: Session):
     job = db.query(Job).filter(Job.id == id).first()
     return job
 
 
+def list_jobs(db: Session):
+    jobs = db.query(Job).filter(Job.is_active==True).all()
+    return jobs
 
 
-     
+def update_job_by_id(id: int, job: JobCreate, db: Session):
+    pass
+
