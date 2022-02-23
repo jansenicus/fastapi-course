5c727a7ff1a266b485d50a1164933fb1be9a4702
Jansen A. Simanullang <jansen.simanullang@gmail.com>
2022-02-23 13:45:41 +0700

    Update route_jobs.py
    + added route /get/{id}/

--- jobboard/backend/apis/v1/route_jobs.py
+++ jobboard/backend/apis/v1/route_jobs.py
@@ -1,10 +1,12 @@
+from os import stat
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
 from backend.db.session import get_db
 from backend.db.models import jobs
 from backend.schemas.jobs import JobCreate, ShowJob
-from backend.db.repository.jobs import create_new_job
+from backend.db.repository.jobs import create_new_job, retrieve_job
+from backend.schemas.jobs import ShowJob
 
 router = APIRouter()
 
@@ -13,4 +15,13 @@ router = APIRouter()
 def create_job(job: JobCreate, db: Session = Depends(get_db)):
     owner_id = 1
     job = create_new_job(job=job, db=db, owner_id=owner_id)
+    return job
+
+
+@router.get("/get/{id}", response_model=ShowJob)
+def retrieve_job_by_id(id: int, db:Session = Depends(get_db)):
+    job = retrieve_job(id=id, db=db)
+    if not job:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"Job with id: {id} does not exist")
     return job
