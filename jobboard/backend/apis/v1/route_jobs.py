cb0327b0d4d98f8d8236b00023b15820202d3fac
Jansen A. Simanullang <jansen.simanullang@gmail.com>
2022-02-24 17:26:56 +0700

    Update route_jobs.py
    + @router.delete("/delete/{id}")

--- jobboard/backend/apis/v1/route_jobs.py
+++ jobboard/backend/apis/v1/route_jobs.py
@@ -1,11 +1,16 @@
+from email import message
 from os import stat
+from webbrowser import get
 from fastapi import (APIRouter, Depends, HTTPException, status)
 from sqlalchemy.orm import Session
 
 from backend.db.session import get_db
 from backend.db.models import jobs
 from backend.schemas.jobs import JobCreate, ShowJob
-from backend.db.repository.jobs import (create_new_job, retrieve_job, list_jobs, update_job_by_id)
+from backend.db.repository.jobs import (create_new_job, retrieve_job,
+                                        list_jobs,
+                                        update_job_by_id,
+                                        delete_job_by_id)
 
 router = APIRouter()
 
@@ -18,25 +23,37 @@ def create_job(job: JobCreate, db: Session = Depends(get_db)):
 
 
 @router.get("/get/{id}", response_model=ShowJob)
-def retrieve_job_by_id(id: int, db:Session = Depends(get_db)):
+def retrieve_job_by_id(id: int, db: Session = Depends(get_db)):
     job = retrieve_job(id=id, db=db)
     if not job:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-        detail=f"Job with id: {id} does not exist")
+                            detail=f"Job with id: {id} does not exist")
     return job
+
 
 @router.get("/all")
 def retrieve_all_jobs(db: Session = Depends(get_db)):
     jobs = list_jobs(db=db)
     return jobs
 
-     
+
 @router.put("/update/{id}")
-def update_job(id: int, job: JobCreate, db: Session=Depends(get_db)):
+def update_job(id: int, job: JobCreate, db: Session = Depends(get_db)):
     owner_id = 1
-    message = update_job_by_id(id = id, job= job, db = db, owner_id = owner_id)
+    message = update_job_by_id(id=id, job=job, db=db, owner_id=owner_id)
     if not message:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-        details = "Job with id {id} does not exists")
+                            details=f"Job with id {id} does not exists")
 
     return {"detail": "Succesfully updated data"}
+
+
+@router.delete("/delete/{id}")
+def delete_job(id: int, db: Session = Depends(get_db)):
+    owner_id = 1
+    message = delete_job_by_id(id=id, db=db, owner_id=owner_id)
+    if not message:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Job with id {id} does not exists.")
+
+    return {"detail": "Succesfully deleted"}
